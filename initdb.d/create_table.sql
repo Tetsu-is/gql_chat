c79070b098036f4bba6bfc4201bfa4f0dbf7b8ee
Tetsu-is <hap30305@kwansei.ac.jp>
2024-05-03 15:56:58 +0900

    add demo data

--- initdb.d/create_table.sql
+++ initdb.d/create_table.sql
@@ -5,6 +5,7 @@ create table users (
     password varchar(255) not null
 );
 
+
 create table messages {
     id int primary key,
     user_id int,
@@ -13,3 +14,9 @@ create table messages {
     created_at timestamp,
     foreign key (user_id) references users(id)
 };
+
+/* insert demo datas */
+insert into messages(id, user_id, user_name, body, created_at) values(1, 1, 'user1', 'hello1', '2021-01-01 00:00:00'),
+(2, 1, 'user1', 'world2', '2021-01-01 00:00:01'),
+(3, 2, 'user2', 'hello3', '2021-01-01 00:00:02'),
+(4, 2, 'user2', 'world4', '2021-01-01 00:00:03');
